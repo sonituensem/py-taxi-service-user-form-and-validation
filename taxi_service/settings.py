67ae4e596d04032e8f1440a4de3584a054771d20
Yatsenko Dmytro <sonituensem@gmail.com>
2026-06-23 13:27:41 +0300

    Remove unnecessary crispy dependency

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -43,8 +43,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "debug_toolbar",
-    "crispy_forms",
-    "crispy_bootstrap4",
     "taxi",
 ]
 
@@ -144,5 +142,3 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CRISPY_TEMPLATE_PACK = "bootstrap4"
